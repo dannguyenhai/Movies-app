43456351bdffa8dfed202400aaa730046cee20fd
Nguyenhaidan <nguyenhaidan13112000@gmail.com>
2023-04-30 02:06:08 +0700

    icon card

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import "swiper/scss";
 import {Route, Routes} from "react-router-dom";
 import Main from "./layout/Main";
 import Banner from "./banner/Banner";
-// import HomePage from "./Page/HomePage";
-// import Movies from "./Page/MoviePage";
-// import MovieDetailsPage from "./Page/MovieDetailsPage";
-// import {NavLink} from "react-router-dom";
 const HomePage = lazy(()=> import("./Page/HomePage"));
 const MovieDetailsPage = lazy(()=> import("./Page/MovieDetailsPage"));
 const Movies = lazy(()=> import("./Page/MoviePage"));
+
 function App() {
   return (
     <Fragment>
